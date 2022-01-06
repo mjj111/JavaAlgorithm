3a3d2adf5dcfe3993257e132b79c62ce432b7f8e
mjj111 <skatks1016@naver.com>
2022-01-06 16:44:11 +0900

    d

---
+++ LV1/문자열을정수로.java
@@ -0,0 +1,10 @@
+package LV1;
+
+public class 문자열을정수로 {
+    class Solution {
+        public int solution(String s) {
+            int answer = Integer.parseInt(s);
+            return answer;
+        }
+    }
+}
